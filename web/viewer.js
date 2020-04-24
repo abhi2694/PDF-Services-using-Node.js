d86f5280649736a6a55a870b2cfc8f8f21832196
Abhinav Kumar <abhi2694@gmail.com>
2020-04-24 22:25:43 +0530

    commit

--- web/viewer.js
+++ web/viewer.js
@@ -3536,7 +3536,7 @@ const defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: "compressed.tracemonkey-pldi-09.pdf",
+    value: "tracemonkey.pdf",
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
